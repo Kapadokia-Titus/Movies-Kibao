c03826b1afd3399b4b4dd0b7ba0ca4af76c9f83a
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-07-26 00:12:03 +0300

    initial commit

---
+++ app/src/main/java/kapadokia/nyandoro/moviekibao/MainActivity.java
@@ -0,0 +1,14 @@
+package kapadokia.nyandoro.moviekibao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
